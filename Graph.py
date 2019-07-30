d8808c5d3367696150dcaeeaa8b0141850ad0664
Chris Lam <loyalty98@gmail.com>
2019-07-30 16:51:08 -0400

    Removed unneeded code

--- Graph.py
+++ Graph.py
@@ -1,8 +1,6 @@
 import itertools
 import heapq
-
-
-
+import datetime
 
 # Greedy implementation. Very subpar. Will change to graph implementation as soon as I can. 
 
@@ -24,8 +22,6 @@ def findOverlap(base, addition):
     while overlap != baseTail:
         overlap = overlap[:-1]
         baseTail = baseTail[1:]
-        print("hi: " + baseTail)
-        print(overlap)
     return addition[len(overlap):]
 
 def difference(base, addition):
@@ -40,10 +36,9 @@ def findSuper():
         overlaps = list(map(lambda x: difference(baseString, x), perms))
         mostOverlapIndex = overlaps.index(min(overlaps))
         baseString = baseString + findOverlap(baseString, perms[mostOverlapIndex])
-        perms = list(filter(lambda x: x not in baseString, perms))
-
-    print(verifySuper(baseString, n))
-    print(len(baseString))
+        perms.pop(mostOverlapIndex)
+    print("Now verifying that all permutations are contained within the string: " + str(verifySuper(baseString, n)))
+    print("The superpermtation is " + str(len(baseString)) + " characters long")
     return baseString
 
 def verifySuper(superCandidate, n):
@@ -53,60 +48,9 @@ def verifySuper(superCandidate, n):
             return False
     return True
 
-
+print(datetime.datetime.now())
 print(findSuper())
-
-
-
-# Graph implementation, hopefully using asymmetrical
-# travelling salesman. TODO: Finish this
-
-# class Node:
-#     def __init__(self, perm):
-#         """Perm is a string that is a permutation of n characters. """
-#         self.value = perm
-#         self.edges = []
-#         self.visited = False
-
-#     def getValue(self):
-#         return self.value
-
-#     def getEdges(self):
-#         return self.edges
-    
-#     def findWeight(self, n2):
-#         """Finds weight from self to n2"""
-#         weight = 0
-#         p1 = self.getValue()
-#         p2 = n2.getValue()
-#         while p1 != p2:
-#             p1 = p1[1:]
-#             p2 = p2[:-1]
-#             weight += 1
-#         return weight
-
-# class Edge:
-    
-#     def __init__(self, n1, n2):
-#         """edge from n1 to n2"""
-#         self.origin = n1
-#         self.destination = n2
-#         self.weight = n1.findWeight(n2)
-    
-#     def __lt__(self, other):
-#         if self.destination
-#         return self.weight < other.weight
-        
-
-
-# class Graph:
-
-
-#     def __init__(self, n):
-#         perms = producePermuations(n)
-#         for index in list(range(perms)):
-#             perms[index] = Node(perms[index])
-#         self.nodes = perms
+print(datetime.datetime.now())
 
 
         
